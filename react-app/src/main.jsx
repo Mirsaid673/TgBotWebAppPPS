6bbc38365d29381b0a9356f9f7a5974f2714e1dc
KirTheGreat <mr.mirsaid899@gmail.com>
2024-03-29 18:10:03 +1000

    vercel button color

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -5,6 +5,24 @@ import "./index.css";
 import { extendTheme, ChakraProvider } from "@chakra-ui/react";
 
 const theme = extendTheme({
+  components: {
+    Button: {
+      // 1. We can update the base styles
+      baseStyle: {
+        fontWeight: 'bold', // Normally, it is "semibold"
+        bg: window.Telegram.WebApp.themeParams.button_color,
+      },
+      // 2. We can add a new button size or extend existing
+      sizes: {
+        xl: {
+          h: '56px',
+          fontSize: 'lg',
+          px: '32px',
+        },
+      },
+    },
+  },
+
   styles: {
     global: {
       // styles for the `body`
