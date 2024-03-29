89ecedf3712e6ab9b53fd5a02de816c79796eef7
KirTheGreat <mr.mirsaid899@gmail.com>
2024-03-29 15:42:22 +1000

    Vercel test commit

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -9,15 +9,6 @@ import { Stack, HStack, VStack } from "@chakra-ui/react";
 import { color } from 'framer-motion'
 import './App.css'
 
-import {
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  AlertDescription,
-  Stack,
-  Code
-} from '@chakra-ui/react'
-
 function App() {
   // 2. Wrap ChakraProvider at the root of your app
   return (
@@ -46,28 +37,6 @@ function App() {
           back
         </Button>
       </VStack>
-    <Stack spacing={3}>
-      <Alert status='error'>
-        <AlertIcon />
-        <div><Code><a href="https://www.farpost.ru" style={{color: "blue"}}>farpost.ru</a></Code> is not in the sudoers file. This incident will be reported.</div>
-      </Alert>
-
-      <Alert status='success'>
-        <AlertIcon />
-        We have a button! Many buttons!
-      </Alert>
-
-      <Alert status='warning'>
-        <AlertIcon />
-        The project defense is in 8 days.
-      </Alert>
-
-      <Alert status='info'>
-        <AlertIcon />
-        <Code><span style={{color: "red"}}>nightSanya@hotel $</span> sudo shutdown -P 23:00</Code>
-      </Alert>
-      
-    </Stack>
     </>
   )
 }
