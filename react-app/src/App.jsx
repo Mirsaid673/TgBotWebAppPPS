441248e1088e6a0d1d4f14caaf18a293b2a1c7f4
KirTheGreat <mr.mirsaid899@gmail.com>
2024-03-29 15:32:15 +1000

    buttons added

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-import { ChakraProvider } from '@chakra-ui/react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { StackDivider, extendTheme, ChakraProvider } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Stack, HStack, VStack } from "@chakra-ui/react";
 
 function App() {
   // 2. Wrap ChakraProvider at the root of your app
   return (
-    <Button colorScheme='blue'>Button</Button>
-  )
+    <>
+      <VStack spacing="10px" align="stretch" justify="center">
+        <HStack spacing="10px">
+          <Button colorScheme="blue" onClick={() => console.log("clicked")}>
+            Button
+          </Button>
+          <Button colorScheme="blue">Button</Button>
+        </HStack>
+
+        <HStack spacing="10px">
+          <Button colorScheme="blue" onClick={() => console.log("clicked")}>
+            Button
+          </Button>
+          <Button colorScheme="blue">Button</Button>
+          <button>button</button>
+        </HStack>
+
+        <Button
+          colorScheme="blue"
+          fill={true}
+          onClick={() => console.log("clicked")}
+        >
+          back
+        </Button>
+      </VStack>
+    </>
+  );
 }
 
-export default App
+export default App;
