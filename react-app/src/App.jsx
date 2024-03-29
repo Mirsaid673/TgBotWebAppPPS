6465e94bd2d3b97e234f52336573b22e063178cb
yokhor <yokhor06@mail.ru>
2024-03-29 15:04:17 +1000

    killer feature added

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { color } from 'framer-motion'
 import './App.css'
 
-import { ChakraProvider } from '@chakra-ui/react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  Stack,
+  Code
+} from '@chakra-ui/react'
 
 function App() {
   // 2. Wrap ChakraProvider at the root of your app
   return (
-    <Button colorScheme='blue'>Button</Button>
+    <Stack spacing={3}>
+      <Alert status='error'>
+        <AlertIcon />
+        <div><Code><a href="https://www.farpost.ru" style={{color: "blue"}}>farpost.ru</a></Code> is not in the sudoers file. This incident will be reported.</div>
+      </Alert>
+
+      <Alert status='success'>
+        <AlertIcon />
+        We have a button! Many buttons!
+      </Alert>
+
+      <Alert status='warning'>
+        <AlertIcon />
+        The project defense is in 8 days.
+      </Alert>
+
+      <Alert status='info'>
+        <AlertIcon />
+        <Code><span style={{color: "red"}}>nightSanya@hotel $</span> sudo shutdown -P 23:00</Code>
+      </Alert>
+      
+    </Stack>
   )
 }
 
