18a4763ef37731841576f1c8c0dc0b402eeae0b7
sawmassacre <103824860+SawMassacre@users.noreply.github.com>
2024-04-01 17:41:41 +1000

    feat: Demo swiper

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MovieHall from "./MovieHall.jsx";
+import EventSwipe from "./EventSwipe.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/movie" element={<MovieHall />} />
+        <Route path="/event" element={<EventSwipe />} />
       </Routes>
     </Router>
   );
---
+++ react-app/src/EventSwipe.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import {
+  Card,
+  CardBody,
+  Stack,
+  Heading,
+  Divider,
+  CardFooter,
+  ButtonGroup,
+  Button,
+  Image, // Import Image from Chakra UI
+  Text, // Import Text from Chakra UI
+} from "@chakra-ui/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+const SwipableCard = () => {
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+    >
+      <SwiperSlide>
+        <Card maxW="sm">
+          <CardBody>
+            <Image
+              src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              alt="Green double couch with wooden legs"
+              borderRadius="lg"
+            />
+            <Stack mt="6" spacing="3">
+              <Heading size="md">Living room Sofa</Heading>
+              <Text>
+                This sofa is perfect for modern tropical spaces, baroque
+                inspired spaces, earthy toned spaces and for people who love a
+                chic design with a sprinkle of vintage design.
+              </Text>
+              <Text color="blue.600" fontSize="2xl">
+                $450
+              </Text>
+            </Stack>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <ButtonGroup spacing="2">
+              <Button variant="solid" colorScheme="blue">
+                Buy now
+              </Button>
+              <Button variant="ghost" colorScheme="blue">
+                Add to cart
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Card maxW="sm">
+          <CardBody>
+            <Image
+              src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              alt="Green double couch with wooden legs"
+              borderRadius="lg"
+            />
+            <Stack mt="6" spacing="3">
+              <Heading size="md">Living room Sofa</Heading>
+              <Text>
+                This is perfect for modern tropical spaces, baroque inspired
+                spaces, eargn.
+              </Text>
+              <Text color="blue.600" fontSize="2xl">
+                $450
+              </Text>
+            </Stack>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <ButtonGroup spacing="2">
+              <Button variant="solid" colorScheme="blue">
+                Buy now
+              </Button>
+              <Button variant="ghost" colorScheme="blue">
+                Add to cart
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default SwipableCard;
