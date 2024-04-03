3ecdcd43dc79dc84f5c01fb99d8275d9ee533c90
DREdward-chell <sushkov2007@gmail.com>
2024-04-03 18:09:04 +1000

    Cringe

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MovieHall from "./MovieHall.jsx";
 import Weather from "./Weather.jsx";
+import SwipableCard from "./EventSwipe.jsx";
 
 function App() {
   // 2. Wrap ChakraProvider at the root of your app
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/movie" element={<MovieHall />} />
         <Route path="/weather" element={<Weather />} />
+        <Route path="/swiper" element={<SwipableCard />} />
       </Routes>
     </Router>
     </div>
