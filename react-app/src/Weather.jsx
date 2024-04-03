e0d11cc910cde4b519f285579ab9a957f0936772
DREdward-chell <sushkov2007@gmail.com>
2024-04-03 14:37:52 +1000

    Simple pages for beta version

--- react-app/src/Weather.jsx
+++ react-app/src/Weather.jsx
@@ -1,64 +1,78 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
-import { Image, Icon } from "@chakra-ui/react";
-import { WiDayCloudy, WiRain, WiSnow, WiDaySunny } from "react-icons/wi";
+import {Image, Icon} from "@chakra-ui/react";
+import {WiDayCloudy, WiRain, WiSnow, WiDaySunny} from "react-icons/wi";
 import "./Weather.css";
+import weather from "./assets/weather/weather.png"
+import water from "./assets/weather/drop.png"
+import pressure from "./assets/weather/pressure.png"
+import therm from "./assets/weather/temperature.png"
+import wind from "./assets/weather/wind.png"
 
 const Weather = () => {
     const getIcon = (type) => {
-        if(type == "Clear") {
+        if (type === "Clear")
             return WiDaySunny;
-        }
-        if(type == "Snow") {
+        if (type === "Snow")
             return WiSnow;
-        }
-        if(type == "Clouds") {
+        if (type === "Clouds")
             return WiDayCloudy;
-        }
-        if(type == "Rain") {
+        if (type === "Rain")
             return WiRain;
-        }
-        return 
     }
 
-  const [weatherData, setWeatherData] = useState(null);
+    const [weatherData, setWeatherData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=Vladivostok&units=metric&lang=ru&appid=9e274a17162b8b27cfc5730a2d8f4fc7`
-        );
-        setWeatherData(response.data);
-      } catch (error) {
-        console.error("Error fetching weather data:", error);
-      }
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    `https://api.openweathermap.org/data/2.5/weather?q=Vladivostok&units=metric&lang=ru&appid=9e274a17162b8b27cfc5730a2d8f4fc7`
+                );
+                setWeatherData(response.data);
+            } catch (error) {
+                console.error("Error fetching weather data:", error);
+            }
+        };
 
-    fetchData();
-  }, []);
+        fetchData();
+    }, []);
 
-  return (
-    <div className="weather-container">
-      {weatherData ? (
-        <>
-          <h1   >Погода во Владивостоке</h1>
-
-          <p>
-            Описание: {weatherData.weather[0].description} <Icon as={getIcon(weatherData.weather[0].main)} />
-          </p>
-          <p>Температура: {weatherData.main.temp}°C</p>
-
-          <p>Ощущается как: {weatherData.main.feels_like}°C</p>
-          <p>Влажность: {weatherData.main.humidity}%</p>
-          <p>Давление: {weatherData.main.pressure}</p>
-          <p>Скорость ветра: {weatherData.wind.speed}м/с</p>
-        </>
-      ) : (
-        <p>Loading weather data...</p>
-      )}
-    </div>
-  );
+    return (
+        <div className="weather-container">
+            {weatherData ? (
+                <>
+                    <h1 className={'headerGap'}>Погода во Владивостоке</h1>
+                    <div className={'weather-property'}>
+                        <img src={weather} alt={weather}></img>
+                        <p>Описание: {weatherData.weather[0].description}</p>
+                    </div>
+                    <div className={'weather-property'}>
+                        <img src={therm} alt={therm}></img>
+                        <p>Температура: {weatherData.main.temp}°C</p>
+                    </div>
+                    <div className={'weather-property'}>
+                        <img src={therm} alt={therm} className={'transparent'}></img>
+                        <p>Ощущается как: {weatherData.main.feels_like}°C</p>
+                    </div>
+                    <div className={'weather-property'}>
+                        <img src={water} alt={water}></img>
+                        <p>Влажность: {weatherData.main.humidity}</p>
+                    </div>
+                    <div className={'weather-property'}>
+                        <img src={pressure} alt={pressure}></img>
+                        <p>Давление: {weatherData.main.pressure}</p>
+                    </div>
+                    <div className={'weather-property'}>
+                        <img src={wind} alt={wind}></img>
+                        <p>Скорость ветра: {weatherData.wind.speed}м/с</p>
+                    </div>
+                </>
+            ) : (
+                <p>Loading weather data...</p>
+            )}
+        </div>
+    );
 };
 
 export default Weather;
