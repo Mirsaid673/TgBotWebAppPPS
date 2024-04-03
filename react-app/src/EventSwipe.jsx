cdba9dd67831f188e955384bf643b274dcdd1a51
DREdward-chell <sushkov2007@gmail.com>
2024-04-03 18:25:59 +1000

    Experiments with vercel

--- react-app/src/EventSwipe.jsx
+++ react-app/src/EventSwipe.jsx
@@ -14,6 +14,7 @@
 import { Virtual } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import "swiper/css";
+import "swiper/css/virtual"
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
