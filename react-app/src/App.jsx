20de200a39187f469ed3bd1357a79e066d47f617
KirTheGreat <mr.mirsaid899@gmail.com>
2024-04-03 10:36:17 +1000

    added weather page

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MovieHall from "./MovieHall.jsx";
-import EventSwipe from "./EventSwipe.jsx";
+import Weather from "./Weather.jsx";
 
 function App() {
   // 2. Wrap ChakraProvider at the root of your app
   return (
+    <div className="app-container">
     <Router>
       <Routes>
         <Route path="/movie" element={<MovieHall />} />
-        <Route path="/event" element={<EventSwipe />} />
+        <Route path="/weather" element={<Weather />} />
       </Routes>
     </Router>
+    </div>
   );
 }
 
--- react-app/src/Weather.jsx
+++ react-app/src/Weather.jsx
@@ -1,41 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Button, HStack, VStack } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Image, Icon } from "@chakra-ui/react";
+import { WiDayCloudy, WiRain, WiSnow, WiDaySunny } from "react-icons/wi";
+import "./Weather.css";
 
-const Weather = () => {React
+const Weather = () => {
+    const getIcon = (type) => {
+        if(type == "Clear") {
+            return WiDaySunny;
+        }
+        if(type == "Snow") {
+            return WiSnow;
+        }
+        if(type == "Clouds") {
+            return WiDayCloudy;
+        }
+        if(type == "Rain") {
+            return WiRain;
+        }
+        return 
+    }
 
- useEffect(() => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=Vladivostok&units=metric&appid=897e706da5f342fa4c61141ac9dad97c`
+          `https://api.openweathermap.org/data/2.5/weather?q=Vladivostok&units=metric&lang=ru&appid=9e274a17162b8b27cfc5730a2d8f4fc7`
         );
         setWeatherData(response.data);
       } catch (error) {
-        console.error('Error fetching weather data:', error);
+        console.error("Error fetching weather data:", error);
       }
     };
 
     fetchData();
- }, []);
+  }, []);
 
- return (
-    <div>
+  return (
+    <div className="weather-container">
       {weatherData ? (
         <>
-          <h2>Weather in Vladivostok</h2>
-          <p>Temperature: {weatherData.main.temp}°C</p>
-          <p>Description: {weatherData.weather[0].description}</p>
-          <p>Feels like: {weatherData.main.feels_like}°C</p>
-          <p>Humidity: {weatherData.main.humidity}%</p>
-          <p>Pressure: {weatherData.main.pressure}</p>
-          <p>Wind Speed: {weatherData.wind.speed}m/s</p>
+          <h1   >Погода во Владивостоке</h1>
+
+          <p>
+            Описание: {weatherData.weather[0].description} <Icon as={getIcon(weatherData.weather[0].main)} />
+          </p>
+          <p>Температура: {weatherData.main.temp}°C</p>
+
+          <p>Ощущается как: {weatherData.main.feels_like}°C</p>
+          <p>Влажность: {weatherData.main.humidity}%</p>
+          <p>Давление: {weatherData.main.pressure}</p>
+          <p>Скорость ветра: {weatherData.wind.speed}м/с</p>
         </>
       ) : (
         <p>Loading weather data...</p>
       )}
     </div>
- );
+  );
 };
 
 export default Weather;
