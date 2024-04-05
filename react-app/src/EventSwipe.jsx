e9b00ac6fece90050d917cbf293424bbb88b09d0
DREdward-chell <sushkov2007@gmail.com>
2024-04-05 11:46:27 +1000

    Alpha

--- react-app/src/EventSwipe.jsx
+++ react-app/src/EventSwipe.jsx
@@ -1,23 +1,30 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCards } from 'swiper/modules';
 import "swiper/css";
 import 'swiper/css/effect-cards';
 import "./swiperStyle.css"
 import MovieHall from "./MovieHall.jsx";
-import films_imported from "../../backend/data/films_vladivostok_04_04_2024.json"
+import films_imported from "../../backend/data/films_vladivostok_05_04_2024.json"
+import films_times from "../../backend/data/seances_vladivostok_05_04_2024.json"
 // now films import is hardcoded, should be done better later
 
 const SwipableCard = function () {
     let films = [...films_imported];
     films = films.sort(() => 0.5 - Math.random());
-    console.log(films);
+
+    const findTime = (id, times) => {
+        for (let i = 0; i < times.length; i++) {
+            if (times[i].nameId === id)
+                return times[i].time
+        }
+        return "10:00"
+    }
 
     return (
         <>
             <Swiper>
               {films.map((film) => (
                 <SwiperSlide key={film.filmId}>
-                  <MovieHall film={film} />
+                  <MovieHall film={film} time={findTime(film.filmId, films_times)} />
                 </SwiperSlide>
               ))}
             </Swiper>
--- react-app/src/MovieHall.jsx
+++ react-app/src/MovieHall.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button, Checkbox } from "@chakra-ui/react";
 import defaultImage from "../src/assets/defaultImage.jpg"
 
@@ -10,12 +10,12 @@ class Seat {
 }
 
 function MovieHall(film) {
-  film = film.film;
+  let filM = film.film;
   
   const columns = 10;
   const rows = 7;
 
-  const image = "https://www.kino.vl.ru/kino/images/" + film.pictureHref;
+  const image = "https://www.kino.vl.ru/kino/images/" + filM.pictureHref;
   console.log(image);
 
   // Define seats as a state variable
@@ -77,19 +77,15 @@ function MovieHall(film) {
     }
     items.push(<div key={i}>{row_items}</div>);
   }
-
-  console.log(image);
-  console.log(defaultImage);
-  console.log(film.pictureHref);
   return (
       <>
         <div>
           <div className={'picture'}>
-            <img src={film.pictureHref !== "" ? image : defaultImage} />
+            <img src={filM.pictureHref !== "" ? image : defaultImage} />
           </div>
-          <h1>{film.name}</h1>
+          <h1>{filM.name}</h1>
           <p className={'centered'}>Новая эра фантастики</p>
-          <h2>18:00</h2>
+          <h2>{film.time}</h2>
         </div>
         <div className="seats-container">{items}</div>
         <Button margin="1rem">Цена {total_price} ₽</Button>
